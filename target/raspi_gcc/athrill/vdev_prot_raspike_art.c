ad9a620614b0aced09ebf3c749a035c0dd89bc1d
Elic0de <lpondboy@gmail.com>
2026-07-22 23:39:21 +0000

    Add robot-devkit RasPike applications

--- target/raspi_gcc/athrill/vdev_prot_raspike_art.c
+++ target/raspi_gcc/athrill/vdev_prot_raspike_art.c
@@ -11,26 +11,11 @@
 #include "vdev_private.h"
 #include "target_kernel_impl.h"
 #include <pthread.h>
+#include <unistd.h>
 // For RasPike-ART
 #include "raspike_protocol_api.h"
 
 static MpthrIdType vdev_thrid;
-
-static sigset_t prev_sigset;
-static void lock_task(void)
-{
-  sigset_t sigset;
-  sigemptyset(&sigset);
-  sigaddset(&sigset,SIGUSR2);
-  sigaddset(&sigset,SIGALRM);
-  sigprocmask(SIG_BLOCK, &sigset, &prev_sigset);
-}
-
-static void unlock_task(void)
-{
-  sigprocmask(SIG_SETMASK,&prev_sigset,0);
-}
-
 
 static Std_ReturnType vdevProtRaspikeSilCb(unsigned int size, uintptr_t addr, const void* data);
 
@@ -44,22 +29,25 @@ static MpthrOperationType vdev_op = {
 
 int vdevProtRaspikeARTInit(const VdevIfComMethod *com)
 {
-  Std_ReturnType err;
-  
-  RPComDescriptor *desc = (RPComDescriptor*)com->info;
+  if (!com || !com->info) return STD_E_INVALID;
+  RPComDescriptor *desc = (RPComDescriptor *)com->info;
   
   /* デバイスIOに書き込んだ際に呼ばれるコールバック関数 */
   SilSetWriteHook((const SilWriteHook)vdevProtRaspikeSilCb);
 
-  raspike_prot_init(desc);
+  int init_result = raspike_prot_init(desc);
+  if (init_result < 0) {
+    fprintf(stderr, "RASPIKE_FATAL,stage=protocol_init,error=%d\n", -init_result);
+    return STD_E_INVALID;
+  }
   
   mpthread_init();
 
-  err = mpthread_register(&vdev_thrid, &vdev_op);
+  Std_ReturnType err = mpthread_register(&vdev_thrid, &vdev_op);
+  if (err != STD_E_OK) return err;
   err = mpthread_start_proc(vdev_thrid);
-
-  err; // for avoid warning
-  return 0;
+  if (err != STD_E_OK) return err;
+  return STD_E_OK;
 }  
 
 
@@ -75,18 +63,15 @@ static void disable_interrupt(sigset_t *old)
   return;
 }
 
-static void enable_interrupt(sigset_t *to_set)
-{
-  sigprocmask(SIG_SETMASK,to_set,NULL);
-}
-
 /* IOメモリへの書き込み */
-Std_ReturnType vdevProtRaspikeSilCb(unsigned int size, uintptr_t  addr, const void *data)
+Std_ReturnType vdevProtRaspikeSilCb(unsigned int size, uintptr_t addr, const void *data)
 {
+  (void)size;
+  (void)data;
   if (addr != VDEV_TX_FLAG(0)) {
     // Called EV3 API. But RasPike-ART does not support it.
   
-    printf("[WARN] RasPike-ART does not support EV3 API(addr = %x)\n",(void*)addr);
+    printf("[WARN] RasPike-ART does not support EV3 API(addr = %p)\n", (void *)addr);
 
   }
   
@@ -97,6 +82,7 @@ Std_ReturnType vdevProtRaspikeSilCb(unsigned int size, uintptr_t  addr, const vo
 /* 受信スレッド */
 static Std_ReturnType vdev_thread_do_init(MpthrIdType id)
 {
+  (void)id;
   /* 受信用スレッドでsignalを受けると、ON_STACKの判定が効かなくなるため、受信プロセスではsignalを受け取らないようにする*/
   disable_interrupt(NULL);
   return STD_E_OK;
@@ -104,11 +90,21 @@ static Std_ReturnType vdev_thread_do_init(MpthrIdType id)
 
 static Std_ReturnType vdev_thread_do_proc(MpthrIdType id)
 {
+  (void)id;
+  unsigned int fatal_errors = 0;
   while(1) {
-    raspike_prot_receive();
+    int result = raspike_prot_receive();
+    if (result == -EPIPE || result == -EIO || result == -ENOTCONN) {
+      if (++fatal_errors >= 3) {
+        fprintf(stderr, "RASPIKE_FATAL,stage=receive,error=%d\n", -result);
+        _exit(75); /* systemd restarts the runtime and re-negotiates a session */
+      }
+      struct timespec delay = {.tv_sec = 0, .tv_nsec = 10000000};
+      nanosleep(&delay, NULL);
+    } else {
+      fatal_errors = 0;
+    }
   }
-
-  enable_interrupt(NULL);
 
   return STD_E_OK;
 
